bf9adebf1b4bb791cab704963d2719872a0e0870
bluesky <bluesky711@outlook.com>
2019-12-14 11:13:54 +0800

    - remove Frontend - clear API

--- resources/assets/js/actions/managelisting/renderSidebarAction.jsx
+++
@@ -1,9 +0,0 @@
-export const renderSidebarAction = {
-    type : 're_render_sidebar',
-    re_render : true
-}
-
-export const renderStopSidebarAction  = {
-    type : 're_render_sidebar',
-    re_render : false
-}
--- resources/views/email/contactus.blade.php
+++
@@ -1,7 +0,0 @@
-Name : {{$contactDetail->name}}
-<hr/>
-Email : {{$contactDetail->email}}
-<hr/>
-Phone Number : {{$contactDetail->phone_number}}
-<hr/>
-Feedback : {{$contactDetail->feedback}}
